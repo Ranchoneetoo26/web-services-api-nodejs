66555dd57a1effc05920e00748ad179f4e36cd7c
antonio neto <antonioneetoo19@gmail.com>
2025-09-08 13:29:18 -0300

    revisando

--- src/Infrastructure/Persistence/Sequelize/SequelizeUserRepository.js
+++ src/Infrastructure/Persistence/Sequelize/SequelizeUserRepository.js
@@ -1,70 +1,47 @@
-const IUserRepository = require('../../../Domain/Repositories/IUserRepository');
+const IUserRepository = require('src/Domain/Repositories/IUserRepository');
 const UserModel = require('./models/UserModel');
-const User = require('../../../Domain/User/User');
-const Password = require('../../../Domain/User/ValueObjects/Password');
+const User = require('src/Domain/User/User');
 
-/**
- * Esta é a implementação concreta da interface IUserRepository.
- * Ela usa o Sequelize para interagir com o banco de dados.
- */
 class SequelizeUserRepository extends IUserRepository {
+  async save(user) {
+    const userData = {
+      id: user.id,
+      name: user.name.value,
+      email: user.email.value,
+      password: user.password.hashedPassword,
+    };
+    await UserModel.create(userData);
+  }
 
-  /**
-   * Método privado para converter os dados do Sequelize para a entidade de domínio 'User'.
-   * @param {UserModel} userModel O objeto do Sequelize.
-   * @returns {User|null} A entidade de domínio User ou null.
-   * @private
-   */
-  _toEntity(userModel) {
+  async findByEmail(email) {
+    const userModel = await UserModel.findOne({ where: { email } });
+
     if (!userModel) {
       return null;
     }
-    const { id, name, email, password: hashedPassword } = userModel.toJSON();
-    const passwordVO = new Password(hashedPassword, true); // O 'true' indica que a senha já está hasheada
-    return new User(name, email, passwordVO, id);
+
+    return User.hydrate(
+      userModel.id,
+      userModel.name,
+      userModel.email,
+      userModel.password // Passando a senha que já está hasheada
+    );
   }
 
-  /**
-   * Salva uma entidade User no banco de dados.
-   * @param {User} userEntity A entidade de domínio.
-   */
-  async save(userEntity) {
-    const userData = {
-      id: userEntity.id,
-      name: userEntity.name.value,
-      email: userEntity.email.value,
-      password: userEntity.password.hashedPassword,
-    };
-
-    const existingUser = await UserModel.findByPk(userData.id);
-
-    if (existingUser) {
-      await existingUser.update(userData);
-    } else {
-      await UserModel.create(userData);
-    }
-  }
-
-  /**
-   * Encontra um usuário pelo seu ID.
-   * @param {string} id
-   * @returns {Promise<User|null>}
-   */
   async findById(id) {
-    const userModel = await UserModel.findByPk(id);
-    return this._toEntity(userModel);
-  }
+const userExists = await this.userRepository.findOneBy({ email: createUserDto.email });;
 
-  /**
-   * Encontra um usuário pelo seu email.
-   * @param {string} email
-   * @returns {Promise<User|null>}
-   */
-  async findByEmail(email) {
-    const userModel = await UserModel.findOne({ where: { email } });
-    return this._toEntity(userModel);
+    if (!userModel) {
+      return null;
+    }
+
+    return User.hydrate(
+      userModel.id,
+      userModel.name,
+      userModel.email,
+      userModel.password
+    );
   }
 }
 
 module.exports = SequelizeUserRepository;
-
